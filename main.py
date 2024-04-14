4e35452df01a1df33864405831fba7c48561ff98
jarvma18 <markusmjarvinen@outlook.com>
2024-04-14 15:19:35 +0300

    Started to implement the code based on the tests.

--- main.py
+++ main.py
@@ -5,3 +5,42 @@ def openFile(fileName: str):
     raise Exception('File not found, check that the file exists in that path')
   else:
     return file
+
+def readFile(file):
+  return file.readlines()
+
+def splitString(string: str):
+  return string.split()
+
+def validateAction(action: str):
+  if action == 'P':
+    return ['select', 'pen']
+  elif action == 'D':
+    return ['item', 'down']
+  elif action == 'U':
+    return ['item', 'up']
+  elif action == 'W':
+    return ['draw', 'west']
+  elif action == 'N':
+    return ['draw', 'north']
+  elif action == 'E':
+    return ['draw', 'east']
+  elif action == 'S':
+    return ['draw', 'south']
+  else:
+    return 'invalid_action'
+
+def interpret(fileName: str):
+  file = openFile(fileName)
+  commands = readFile(file)
+  for command in commands:
+    value = splitString(command)
+    print(value)
+    action = validateAction(value)
+    if action == 'invalid_action':
+      print('Invalid action')
+    else:
+      print(f'\n{action[0].capitalize()}ing {action[1]} {value}cm')
+
+if __name__ == '__main__':
+  interpret('commands.txt')
