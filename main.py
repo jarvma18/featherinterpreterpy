eb4bb2a044b160bbd79906d82c73a4f801ec0091
jarvma18 <markusmjarvinen@outlook.com>
2024-04-15 00:19:32 +0300

    Added execute logics to program.

--- main.py
+++ main.py
@@ -29,18 +29,37 @@ def validateAction(action: str):
     return ['draw', 'south']
   else:
     return 'invalid_action'
+  
+def executeSelectingAction(item: str, itemNumber: str):
+  return 'Selecting ' + item + ' ' + itemNumber
+
+def executeItemDownOrUpAction(item: str, upOrDown: str):
+  return 'Putting ' + item + ' ' + upOrDown
+
+def executeDrawingAction(direction: str, length: int, unit: str):
+  return 'Drawing ' + length + unit + ' to' + ' ' + direction
+
+def executeActions(action: list, actionNumber: str, currentSelectedItem: str):
+  if action == 'invalid_action':
+    print('Invalid action')
+  elif action[0] == 'select':
+    currentSelectedItem = action[1]
+    print(executeSelectingAction(action[1], actionNumber))
+  elif action[0] == 'item':
+    print(executeItemDownOrUpAction(currentSelectedItem, action[1]))
+  elif action[0] == 'draw':
+    print(executeDrawingAction(action[1], actionNumber, 'cm'))
 
 def interpret(fileName: str):
-  file = openFile(fileName)
-  commands = readFile(file)
+  file: __file__ = openFile(fileName)
+  commands: list = readFile(file)
   for command in commands:
-    value = splitString(command)
-    print(value)
-    action = validateAction(value)
-    if action == 'invalid_action':
-      print('Invalid action')
-    else:
-      print(f'\n{action[0].capitalize()}ing {action[1]} {value}cm')
+    value: list = splitString(command)
+    action: str = value[0]
+    actionNumber: str = value[1]
+    action: list = validateAction(action)
+    currentSelectedItem: str = ''
+    executeActions(action, actionNumber, currentSelectedItem)
 
 if __name__ == '__main__':
   interpret('commands.txt')
