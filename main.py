629a1a7c032ed04dcb54a64c416fca99a22aa480
jarvma18 <markusmjarvinen@outlook.com>
2024-04-18 00:45:54 +0300

    Changed main func so that when calling with internal interpreter we are not taking filename.

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import sys
 
 def main():
   arguments: list = sys.argv[1:]
-  if len(arguments) < 2:
-    print('Usage: python main.py <filename> <interpretter>')
+  if len(arguments) < 1:
+    print('Usage: python main.py <interpretter> [<filename>]')
     sys.exit(1)
-  fileName: str = arguments[0]
-  interpretter: str = arguments[1]
+  interpretter: str = arguments[0]
   if interpretter == 'external':
     from interpretters.external import interpret
+    if len(arguments) < 2:
+      print('Usage: python main.py external <filename>')
+      sys.exit(1)
+    fileName: str = arguments[1]
   elif interpretter == 'internal':
     from interpretters.internal import interpret
+    fileName: str = None
   else:
     print('Invalid interpretter')
     sys.exit(1)
