32a12c05a8dd7b73b7ec59faf2c05857d4bcd877
jarvma18 <markusmjarvinen@outlook.com>
2024-04-18 00:13:14 +0300

    Added functionality for user arguments so user can tell the file and interpreter mode.

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     sys.exit(1)
   fileName: str = arguments[0]
   interpretter: str = arguments[1]
-  
+  if interpretter == 'external':
+    from interpretters.external import interpret
+  elif interpretter == 'internal':
+    from interpretters.internal import interpret
+  else:
+    print('Invalid interpretter')
+    sys.exit(1)
+  interpret(fileName)
 
 if __name__ == "__main__":
   main()
