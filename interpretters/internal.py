29aeab4c5807bccd4f11c766f2774a57d14eff98
jarvma18 <markusmjarvinen@outlook.com>
2024-04-21 00:39:14 +0300

    Added internal interpret Tasks class with functions.

--- interpretters/internal.py
+++ interpretters/internal.py
@@ -1,2 +1,24 @@
-def interpret(file):
-  print('Coming soon')
+class Tasks():
+  def __init__(self):
+    pass
+
+  def selectPen(self, penNumber: str):
+    print('Selecting pen ' + penNumber)
+
+  def itemDown(self, item: str):
+    print('Putting ' + item + ' down')
+
+  def itemUp(self, item: str):
+    print('Putting ' + item + ' up')
+
+  def draw(self, direction: str, length: str, unit: str):
+    print('Drawing ' + length + unit + ' to ' + direction)
+
+def internalInterpret():
+  Tasks().selectPen('2')
+  Tasks().itemDown('pen')
+  Tasks().draw('west', '2', 'cm')
+  Tasks().draw('north', '1', 'cm')
+  Tasks().draw('east', '2', 'cm')
+  Tasks().draw('south', '1', 'cm')
+  Tasks().itemUp('pen')
