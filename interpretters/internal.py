2cf9e4333c0e5e9c93f151a52ec44ca38f39a5c0
jarvma18 <markusmjarvinen@outlook.com>
2024-05-09 13:10:15 +0300

    Changed camelCase to snake_case in vars and funcs.

--- interpretters/internal.py
+++ interpretters/internal.py
@@ -2,23 +2,23 @@ class Tasks():
   def __init__(self):
     pass
 
-  def selectPen(self, penNumber: str):
-    print('Selecting pen ' + penNumber)
+  def select_pen(self, pen_number: str):
+    print('Selecting pen ' + pen_number)
 
-  def itemDown(self, item: str):
+  def item_down(self, item: str):
     print('Putting ' + item + ' down')
 
-  def itemUp(self, item: str):
+  def item_up(self, item: str):
     print('Putting ' + item + ' up')
 
   def draw(self, direction: str, length: str, unit: str):
     print('Drawing ' + length + unit + ' to ' + direction)
 
-def internalInterpret():
-  Tasks().selectPen('2')
-  Tasks().itemDown('pen')
+def internal_interpret():
+  Tasks().select_pen('2')
+  Tasks().item_down('pen')
   Tasks().draw('west', '2', 'cm')
   Tasks().draw('north', '1', 'cm')
   Tasks().draw('east', '2', 'cm')
   Tasks().draw('south', '1', 'cm')
-  Tasks().itemUp('pen')
+  Tasks().item_up('pen')
