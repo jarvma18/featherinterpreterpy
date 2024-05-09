ed96120f0a59c000a0147c76b6f9c3e0573d6a12
jarvma18 <markusmjarvinen@outlook.com>
2024-05-09 13:09:57 +0300

    Changed camelCase to snake_case in vars and funcs.

--- tests.py
+++ tests.py
@@ -2,55 +2,55 @@ import unittest
 import io
 import sys
 
-from interpretters.external import openFile
-from interpretters.external import externalInterpret
-from interpretters.external import splitString
-from interpretters.external import validateAction
-from interpretters.external import executeSelectingAction
-from interpretters.external import executeItemDownOrUpAction
-from interpretters.external import executeDrawingAction
-from interpretters.external import executeActions
+from interpretters.external import open_file
+from interpretters.external import external_interpret
+from interpretters.external import split_string
+from interpretters.external import validate_action
+from interpretters.external import execute_selecting_action
+from interpretters.external import execute_item_down_or_up_action
+from interpretters.external import execute_drawing_action
+from interpretters.external import execute_actions
 
-from interpretters.internal import internalInterpret
+from interpretters.internal import internal_interpret
 from interpretters.internal import Tasks
 
 class InternalInterpretTestClass(unittest.TestCase):
-  def test_selectPen(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    expectedValue: str = 'Selecting pen 2\n'
-    Tasks().selectPen('2')
+  def test_select_pen(self):
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    expected_value: str = 'Selecting pen 2\n'
+    Tasks().select_pen('2')
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_itemDown(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    expectedValue: str = 'Putting pen down\n'
-    Tasks().itemDown('pen')
+  def test_item_down(self):
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    expected_value: str = 'Putting pen down\n'
+    Tasks().item_down('pen')
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_itemUp(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    expectedValue: str = 'Putting pen up\n'
-    Tasks().itemUp('pen')
+  def test_item_up(self):
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    expected_value: str = 'Putting pen up\n'
+    Tasks().item_up('pen')
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
   def test_draw(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    expectedValue: str = 'Drawing 2cm to west\n'
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    expected_value: str = 'Drawing 2cm to west\n'
     Tasks().draw('west', '2', 'cm')
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
   def test_interpret(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    expectedValue: str = \
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    expected_value: str = \
       'Selecting pen 2\n' + \
       'Putting pen down\n' + \
       'Drawing 2cm to west\n' + \
@@ -58,29 +58,29 @@ class InternalInterpretTestClass(unittest.TestCase):
       'Drawing 2cm to east\n' + \
       'Drawing 1cm to south\n' + \
       'Putting pen up\n'
-    internalInterpret()
+    internal_interpret()
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
 class ExternalInterpretTestClass(unittest.TestCase):
   def test_existing_file(self):
     file_name = 'testing.txt'
-    file = openFile(file_name)
+    file = open_file(file_name)
     self.assertIsNotNone(file)
     file.close()
 
-  def test_handleErrorWhenFileNotExists(self):
-    expectionMessage: str = 'File not found, check that the file exists in that path'
-    testFile: str = 'File that does not exist'
+  def test_handle_error_when_file_not_exists(self):
+    exception_message: str = 'File not found, check that the file exists in that path'
+    test_file: str = 'File that does not exist'
     with self.assertRaises(Exception) as context:
-      openFile(testFile)
-    self.assertTrue(expectionMessage in str(context.exception))
+      open_file(test_file)
+    self.assertTrue(exception_message in str(context.exception))
 
   def test_interpret(self):
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
     file: str = 'tasks.txt'
-    expectedValue: str = \
+    expected_value: str = \
       'Selecting pen 2\n' + \
       'Putting pen down\n' + \
       'Drawing 2cm to west\n' + \
@@ -88,151 +88,151 @@ class ExternalInterpretTestClass(unittest.TestCase):
       'Drawing 2cm to east\n' + \
       'Drawing 1cm to south\n' + \
       'Putting pen up\n'
-    externalInterpret(file)
+    external_interpret(file)
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_splitString(self):
+  def test_split_string(self):
     string: str = 'P 2'
-    expectedValue: list = ['P', '2']
-    result: list = splitString(string)
-    self.assertEqual(expectedValue, result)
+    expected_value: list = ['P', '2']
+    result: list = split_string(string)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction1(self):
+  def test_validate_action1(self):
     action: str = 'P'
-    expectedValue: str = ['select', 'pen']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['select', 'pen']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction2(self):
+  def test_validate_action2(self):
     action: str = 'D'
-    expectedValue: str = ['item', 'down']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['item', 'down']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction3(self):
+  def test_validate_action3(self):
     action: str = 'W'
-    expectedValue: str = ['draw', 'west']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['draw', 'west']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction4(self):
+  def test_validate_action4(self):
     action: str = 'N'
-    expectedValue: str = ['draw', 'north']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['draw', 'north']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction5(self):
+  def test_validate_action5(self):
     action: str = 'E'
-    expectedValue: str = ['draw', 'east']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['draw', 'east']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction6(self):
+  def test_validate_action6(self):
     action: str = 'S'
-    expectedValue: str = ['draw', 'south']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['draw', 'south']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_validateAction7(self):
+  def test_validate_action7(self):
     action: str = 'U'
-    expectedValue: str = ['item', 'up']
-    result: str = validateAction(action)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = ['item', 'up']
+    result: str = validate_action(action)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction8(self):
+  def test_execute_action8(self):
     item: str = 'pen'
-    penNumberToSelect: str = '2'
-    expectedValue: str = 'Selecting pen 2'
-    result: str = executeSelectingAction(item, penNumberToSelect)
-    self.assertEqual(expectedValue, result)
+    pen_number_to_select: str = '2'
+    expected_value: str = 'Selecting pen 2'
+    result: str = execute_selecting_action(item, pen_number_to_select)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction9(self):
-    upOrDown: str = 'down'
+  def test_execute_action9(self):
+    up_or_down: str = 'down'
     item: str = 'pen'
-    expectedValue: str = 'Putting pen down'
-    result: str = executeItemDownOrUpAction(item, upOrDown)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Putting pen down'
+    result: str = execute_item_down_or_up_action(item, up_or_down)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction10(self):
-    upOrDown: str = 'up'
+  def test_execute_action10(self):
+    up_or_down: str = 'up'
     item: str = 'pen'
-    expectedValue: str = 'Putting pen up'
-    result: str = executeItemDownOrUpAction(item, upOrDown)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Putting pen up'
+    result: str = execute_item_down_or_up_action(item, up_or_down)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction11(self):
+  def test_execute_action11(self):
     direction: str = 'west'
     length: str = '2'
     unit: str = 'cm'
-    expectedValue: str = 'Drawing 2cm to west'
-    result: str = executeDrawingAction(direction, length, unit)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Drawing 2cm to west'
+    result: str = execute_drawing_action(direction, length, unit)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction12(self):
+  def test_execute_action12(self):
     direction: str = 'north'
     length: str = '1'
     unit: str = 'cm'
-    expectedValue: str = 'Drawing 1cm to north'
-    result: str = executeDrawingAction(direction, length, unit)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Drawing 1cm to north'
+    result: str = execute_drawing_action(direction, length, unit)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction13(self):
+  def test_execute_action13(self):
     direction: str = 'east'
     length: str = '2'
     unit: str = 'cm'
-    expectedValue: str = 'Drawing 2cm to east'
-    result: str = executeDrawingAction(direction, length, unit)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Drawing 2cm to east'
+    result: str = execute_drawing_action(direction, length, unit)
+    self.assertEqual(expected_value, result)
 
-  def test_executeAction14(self):
+  def test_execute_action14(self):
     direction: str = 'south'
     length: str = '1'
     unit: str = 'cm'
-    expectedValue: str = 'Drawing 1cm to south'
-    result: str = executeDrawingAction(direction, length, unit)
-    self.assertEqual(expectedValue, result)
+    expected_value: str = 'Drawing 1cm to south'
+    result: str = execute_drawing_action(direction, length, unit)
+    self.assertEqual(expected_value, result)
 
-  def test_executeActions1(self):
+  def test_execute_actions1(self):
     action: list = ['select', 'pen']
-    actionNumber: str = '2'
-    currentSelectedItem: str = ''
-    expectedValue: str = 'Selecting pen 2\n'
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    executeActions(action, actionNumber, currentSelectedItem)
+    action_number: str = '2'
+    current_selected_item: str = ''
+    expected_value: str = 'Selecting pen 2\n'
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    execute_actions(action, action_number, current_selected_item)
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_executeActions2(self):
+  def test_execute_actions2(self):
     action: list = ['item', 'down']
-    actionNumber: str = '2'
-    currentSelectedItem: str = 'pen'
-    expectedValue: str = 'Putting pen down\n'
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    executeActions(action, actionNumber, currentSelectedItem)
+    action_number: str = '2'
+    current_selected_item: str = 'pen'
+    expected_value: str = 'Putting pen down\n'
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    execute_actions(action, action_number, current_selected_item)
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_executeActions3(self):
+  def test_execute_actions3(self):
     action: list = ['draw', 'west']
-    actionNumber: str = '2'
-    currentSelectedItem: str = ''
-    expectedValue: str = 'Drawing 2cm to west\n'
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    executeActions(action, actionNumber, currentSelectedItem)
+    action_number: str = '2'
+    current_selected_item: str = ''
+    expected_value: str = 'Drawing 2cm to west\n'
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    execute_actions(action, action_number, current_selected_item)
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
 
-  def test_executeActions4(self):
+  def test_execute_actions4(self):
     action: list = 'invalid_action'
-    actionNumber: str = '2'
-    currentSelectedItem: str = ''
-    expectedValue: str = 'Invalid action\n'
-    capturedOutput = io.StringIO()
-    sys.stdout = capturedOutput
-    executeActions(action, actionNumber, currentSelectedItem)
+    action_number: str = '2'
+    current_selected_item: str = ''
+    expected_value: str = 'Invalid action\n'
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    execute_actions(action, action_number, current_selected_item)
     sys.stdout = sys.__stdout__
-    self.assertEqual(expectedValue, capturedOutput.getvalue())
+    self.assertEqual(expected_value, captured_output.getvalue())
